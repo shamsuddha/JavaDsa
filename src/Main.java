471ecf53c7ff9aa72841f871128cd13a2a7ca1b5
shamsuddha <shamsuddha@gmail.com>
2023-10-27 09:19:14 +0600

    Java ongoing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("bangladesh is a good country");
+        System.out.println("bangladesh is a good country");
+
     }
+
 }
