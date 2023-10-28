48ca0998fbfb376616f55406a505cb484d56b5f1
shamsuddha <shamsuddha@gmail.com>
2023-10-28 07:41:00 +0600

    ss

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+        HashMap<String, Integer> scores = new HashMap<>();
 
-        System.out.println("bangladesh is a good country");
-        System.out.println("bangladesh is a good country");
+        HashMap<String, Integer> rajit = new HashMap<>();
+
+        rajit.put("alice", 50);
+        rajit.put("hello", 55);
+        rajit.put("bangladesh", 60);
+        rajit.put("new rajit", 10);
+
+        int rajitNew = rajit.get("hello");
+
+        System.out.println(rajitNew);
 
     }
-
 }
